f763f14a752a04d77828f56763ac8dbee9e4e9e5
David Corrales <corralesdev@gmail.com>
2022-06-21 04:58:26 +0200

    función createDialogText implementada

--- game-mastermind/objetos/app.js
+++ game-mastermind/objetos/app.js
@@ -2,25 +2,48 @@ const { Console } = require("console-mpds");
 const console = new Console();
 playMasterMain();
 
-function playMasterMain(){
+function playMasterMain() {
+    let repeatGame;
     do {
         playGame();
-      } while (repeatGame());
-      console.writeln("Bye! See you next time");
+        repeatGame = createYesNoDialog('Quieres jugar de nuevo?');
+   } while (repeatGame.read());
+    console.writeln("¡Hasta pronto!");
 }
 
-function playGame(){
-    let game ={
-        MAX_ATTEMPTS : 10,
-        CODE_LENGTH : 4,
-        totalPlayers ,
-        secretCode : getSecretCode(CODE_LENGTH,totalPlayers),
+function playGame() {
+    let game = {
+        MAX_ATTEMPTS: 10,
+        CODE_LENGTH: 4,
+        totalPlayers : 2,
+        //secretCode: getSecretCode(CODE_LENGTH, totalPlayers),
 
     }
 }
 
+function createYesNoDialog(dialogText) {
+    return {
+        afirmative: true,
+        negative: false,
+        read: function () {
+            let error;
+            do {
+                let answer = console.readString(`${dialogText} ("si/no"):`);
+                if (answer === 'si') {
+                    return this.afirmative;
+                }
+                if (answer === 'no') {
+                    return this.negative;
+                }
+                console.writeln('Has introducido una respuesta incorrecta');
+                error = true;
+            }while(error);
+        }
+    }
+}
 
-function repeatGame() {
+
+/* function repeatGame() {
     let repeat;
     let validResponse;
     do {
@@ -32,4 +55,4 @@ function repeatGame() {
         console.writeln("\nPor favor, introduce una respuesta correcta.");
       }
     } while (!validResponse);
-  }
+  } */
